548a00a1ab7f87403e375657ebf575d664691ba6
Nicholas Riegel <nicholaspriegel@gmail.com>
2026-02-11 13:03:28 +0100

    .

--- Graphs/AdjacencyList.py
+++ Graphs/AdjacencyList.py
@@ -3,59 +3,63 @@ class AdjacencyList:
     # constructor
     # creates the dictionary
     def __init__(self):
-        self.adjacencyListSet = {}
+        self.mainDictOfSets = {}
 
     # to string method
     # returns the stringified dictionary
     def __str__(self):
-        return str(self.adjacencyListSet)
+        return str(self.mainDictOfSets)
 
     # add vertex method
+    # using set instead of list to optimize out duplicates
     def add_vertex(self, vertex):
-        if vertex not in self.adjacencyListSet:
-            self.adjacencyListSet[vertex] = set()
+        if vertex not in self.mainDictOfSets:
+            self.mainDictOfSets[vertex] = set()
 
     # add edge method
     def add_edge(self, v1, v2):
-        if v1 not in self.adjacencyListSet:
+        if v1 not in self.mainDictOfSets:
             self.add_vertex(v1)
-        if v2 not in self.adjacencyListSet:
+        if v2 not in self.mainDictOfSets:
             self.add_vertex(v2)
 
-        self.adjacencyListSet[v1].add(v2)
-        self.adjacencyListSet[v2].add(v1)
+        self.mainDictOfSets[v1].add(v2)
+        self.mainDictOfSets[v2].add(v1)
 
     # has edge between two verticies check method
     def have_edge(self, v1, v2):
-        if v1 not in self.adjacencyListSet or v2 not in self.adjacencyListSet:
+        if v1 not in self.mainDictOfSets or v2 not in self.mainDictOfSets:
             return False
-        return v1 in self.adjacencyListSet[v2]
+        return v1 in self.mainDictOfSets[v2]
         
     # remove edge method
     def remove_edge(self, v1, v2):
         if self.have_edge(v1, v2):
-            self.adjacencyListSet[v1].discard(v2)
-            self.adjacencyListSet[v2].discard(v1)
+            self.mainDictOfSets[v1].discard(v2)
+            self.mainDictOfSets[v2].discard(v1)
             return True
         else:
             return False
         
     # remove vertext method
     def remove_vertex(self, vertex):
-        if vertex not in self.adjacencyListSet:
+        if vertex not in self.mainDictOfSets:
             return False
         
-        for v in self.adjacencyListSet[vertex]:
-            self.adjacencyListSet[v].discard(vertex)
+        for v in self.mainDictOfSets[vertex]:
+            self.mainDictOfSets[v].discard(vertex)
         
-        del self.adjacencyListSet[vertex]
+        del self.mainDictOfSets[vertex]
         return True
         
     # get verticies method
     def get_vertices(self):
-        return list(self.adjacencyListSet.keys())
+        return list(self.mainDictOfSets.keys())
 
 
+#========
+# Tests
+#========
 al = AdjacencyList()
 al.add_vertex("Hello")
 al.add_edge("Hello", "World")
