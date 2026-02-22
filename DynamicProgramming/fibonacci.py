7911345ee7ccc0f83943771cdd9213ad6ffbbbe5
Nicholas Riegel <nicholaspriegel@gmail.com>
2026-02-22 11:50:57 +0100

    .

--- DynamicProgramming/fibonacci.py
+++ DynamicProgramming/fibonacci.py
@@ -3,22 +3,18 @@
 def fib_it(n):
     if n < 0:
         return "n too small"
-    elif n == 0:
-        return [0]
-    elif n == 1:
-        return [0, 1]
+    elif n <= 1:
+        return n
     else:
         first, second, new = 0, 1, None
-        series = [first, second]
-        n -= 2
+        n -= 1
         for _ in range(n):
             new = first + second
-            series.append(new)
             first = second
             second = new
-        return series
+        return new
 
-print(fib_it(5))
+# print(fib_it(0))
 
 # recursive fibonacci
 # this has a terrible time complexity O(2^n)
@@ -41,7 +37,8 @@ def fib_rec(n):
 def fibRecToList(n):
     series = []
     for i in range(n):
-        series.append(fib_rec(i))
+        # series.append(fib_rec(i))
+        series.append(fib_it(i))
     return series
 
-print(fibRecToList(5))
+print(fibRecToList(8))
