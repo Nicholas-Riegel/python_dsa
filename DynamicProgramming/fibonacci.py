cd9676934615e86d5bf2c6060fa1acf453266812
Nicholas Riegel <nicholaspriegel@gmail.com>
2026-02-24 14:24:15 +0100

    Fib with tabularization dynamic programming.

--- DynamicProgramming/fibonacci.py
+++ DynamicProgramming/fibonacci.py
@@ -44,11 +44,51 @@ def fibRecMem(n, memo={0:0, 1:1}):
         memo[n] = result
         return result 
 
+# tabulated fibonacci
+# this is "bottom" up dynamic programming
+# this should return the fib number at the index
+# O(n)
+def fibTab(n): # 3
+    # guard
+    if n <= 1:
+        return n
+    # table list
+    table = []
+    # populate table list for n+1
+    # so that table[n] gives the correct fib number at index of n
+    # so f(0)=0, f(1)=1, f(2)=1, f(3)=2
+    for i in range(n+1): # [0, 1, 2, 3]
+        if i <= 1:
+            table.append(i) # [0, 1]
+        else:
+            table.append(table[-1] + table[-2]) # [0, 1, 1, 2]
+    return table[n]
+
+# chat's solution
+# pre allocates memory so slightly more performant
+# O(n)
+def fibTabChat(n):
+
+    if n <= 1:
+        return n
+    
+    table = [0] * (n + 1)  # table[i] = fibonacci number i
+    # [0] * 3        # [0, 0, 0]
+    # ['x'] * 2      # ['x', 'x']
+    table[0] = 0
+    table[1] = 1
+    
+    for i in range(2, n + 1):
+        table[i] = table[i-1] + table[i-2]
+    
+    return table[n]  # Return just the nth fibonacci number
 
 # fib to list
 def fibToList(n): 
     # return [fibIt(i) for i in range(n)]
     # return [fibRec(i) for i in range(n)]
-    return [fibRecMem(i) for i in range(n)]
+    # return [fibRecMem(i) for i in range(n)]
+    return [fibTab(i) for i in range(n)]
 
-print(fibToList(45))
+print(fibToList(10))
+print(fibTab(4))
